84383e96735849d61192077c41b1aa34f93e5ea5
flakefish <lukas.perschau@gmail.com>
2024-03-30 16:44:37 +0100

    Jetzt vielleicht?

---
+++ poem.js
@@ -0,0 +1,23 @@
+"use strict";
+var Zufallsgedicht;
+(function (Zufallsgedicht) 
+{
+    let subjekt = ["Artemis", "Hades", "Zagreus", "Cerberus", "Persephone", "Poseidon"];
+    let prädikat = ["jagt", "hasst", "kuschelt", "füttert", "spielt mit", "sucht nach"];
+    let objekt = ["zwei Wölfe", "Hunde", "Katzen", "Schneeleoparden", " große Elefanten", "Robben"];
+    for (let i = subjekt.length; i >= 1; i--) 
+    {
+        let wort = getwort(subject, prädikat, objekt);
+        console.log(wort);
+    }
+    function getwort(_subjekt, _prädikat, _objekt) 
+    {
+        let wort = "";
+        let randomNumber1 = Math.floor(Math.random() * _subjekt.length);
+        randomNumber2 = Math.floor(Math.random() * _prädikat.length);
+        randomNumber3 = Math.floor(Math.random() * _objekt.length);
+        wort = _subjekt.splice(randomNumber1, 1 )[0] + _prädikat.splice(randomNumber2, 1)[0] + _objekt.splice(randomNumber3, 1)[0];
+        return wort;
+    }
+})(Zufallsgedicht || (Zufallsgedicht = {}));
+//# sourceMappingURL=script.js.map
---
+++ poem.ts
@@ -0,0 +1,21 @@
+namespace ZufallsGedicht {
+
+    let subjekt = ["Artemis", "Hades", "Zagreus", "Cerberus", "Persephone", "Poseidon"];
+    let prädikat = ["jagt", "hasst", "kuschelt", "füttert", "spielt mit", "sucht nach"];
+    let objekt = ["zwei Wölfe", "Hunde", "Katzen", "Schneeleoparden", " große Elefanten", "Robben"];
+    
+    for (let i = subjekt.length; i >= 1; i--){
+        let wort = getwort(subjekt, prädikat, objekt);
+        console.log(wort);
+    }
+    
+    function getwort(_subjekt: string[],_prädikat: string[],_objekt: string[]) 
+    {
+        let wort: string ="";
+        let thisSubjekt: number = Math.floor(Math.random() * _subjekt.length);
+        let thisPrädikat: number = Math.floor(Math.random() * _prädikat.length);
+        let thisObjekt: number = Math.floor(Math.random() * _objekt.length);
+        wort = _subjekt.splice(thisSubjekt, 1)[0] + _prädikat.splice(thisPrädikat, 1)[0] + _objekt.splice(thisObjekt, 1)[0];
+        return wort;
+    }
+    }
